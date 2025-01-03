7c51d4e3478687d90321fb8128524a7518d5207b
rexvel <rexvelele@gmail.com>
2025-01-03 20:03:15 +0200

    fix: utils

--- utils/index.js
+++ utils/index.js
@@ -3,5 +3,5 @@ import path from 'path';
 
 export async function readInput(filename, dirname) {
     const input = fs.readFileSync(path.join(dirname)+`/${filename}`, 'utf-8');
-    return Promise.resolve(input);
+    return input;
 }
